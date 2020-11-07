a42f1befe031a616f3b0be4fd3818619d24599e5
Raphaël Gilles <rgilles@student.42.fr>
2020-11-07 16:07:51 +0100

    normified bmp

--- srcs/create_bmp.c
+++ srcs/create_bmp.c
@@ -5,57 +5,59 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rgilles <rgilles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/28 14:29:24 by rgilles           #+#    #+#             */
-/*   Updated: 2020/10/28 14:29:26 by rgilles          ###   ########.fr       */
+/*   Created: 2020/11/07 16:06:17 by rgilles           #+#    #+#             */
+/*   Updated: 2020/11/07 16:06:18 by rgilles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
 
-void createBitmapFileHeader (int nblines, int lineweight, int fd)
+void	file_header(int nblines, int lineweight, int fd)
 {
-    int fileSize = 14 + 40 + lineweight * nblines;
+	unsigned char	header[14];
+	int				size;
 
-    static unsigned char fileHeader[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-
-    fileHeader[ 0] = (unsigned char)('B');
-    fileHeader[ 1] = (unsigned char)('M');
-    fileHeader[ 2] = (unsigned char)(fileSize      );
-    fileHeader[ 3] = (unsigned char)(fileSize >>  8);
-    fileHeader[ 4] = (unsigned char)(fileSize >> 16);
-    fileHeader[ 5] = (unsigned char)(fileSize >> 24);
-    fileHeader[10] = (unsigned char)(14 + 40);
-    write(fd, fileHeader, 14);
+	ft_bzero(&header, 14);
+	size = 14 + 40 + lineweight * nblines;
+	header[0] = (unsigned char)('B');
+	header[1] = (unsigned char)('M');
+	header[2] = (unsigned char)(size);
+	header[3] = (unsigned char)(size >> 8);
+	header[4] = (unsigned char)(size >> 16);
+	header[5] = (unsigned char)(size >> 24);
+	header[10] = (unsigned char)(14 + 40);
+	write(fd, header, 14);
 }
 
-void createBitmapInfoHeader (int height, int width, int fd)
+void	info_header(int height, int width, int fd)
 {
-    static unsigned char infoHeader[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-                                        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    infoHeader[ 0] = (unsigned char)(40);
-    infoHeader[ 4] = (unsigned char)(width      );
-    infoHeader[ 5] = (unsigned char)(width >>  8);
-    infoHeader[ 6] = (unsigned char)(width >> 16);
-    infoHeader[ 7] = (unsigned char)(width >> 24);
-    infoHeader[ 8] = (unsigned char)(height      );
-    infoHeader[ 9] = (unsigned char)(height >>  8);
-    infoHeader[10] = (unsigned char)(height >> 16);
-    infoHeader[11] = (unsigned char)(height >> 24);
-    infoHeader[12] = (unsigned char)(1);
-    infoHeader[14] = (unsigned char)(4*8);
-    write(fd, infoHeader, 40);
+	unsigned char	header[40];
+
+	ft_bzero(&header, 40);
+	header[0] = (unsigned char)(40);
+	header[4] = (unsigned char)(width);
+	header[5] = (unsigned char)(width >> 8);
+	header[6] = (unsigned char)(width >> 16);
+	header[7] = (unsigned char)(width >> 24);
+	header[8] = (unsigned char)(height);
+	header[9] = (unsigned char)(height >> 8);
+	header[10] = (unsigned char)(height >> 16);
+	header[11] = (unsigned char)(height >> 24);
+	header[12] = (unsigned char)(1);
+	header[14] = (unsigned char)(4 * 8);
+	write(fd, header, 40);
 }
 
-void create_bmp (char* image, int height, int width)
+void	create_bmp(char *image, int height, int width)
 {
-    int				fd;
-    int				i;
+	int	fd;
+	int	i;
 
-    fd = open("image.bmp", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    createBitmapFileHeader(height, width * 4, fd);
-    createBitmapInfoHeader(height, width, fd);
-    i = height;
-    while (i-- >= 0)
-        write(fd, image + (i * width * 4), width * 4);
-    close(fd);
+	fd = open("image.bmp", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	file_header(height, width * 4, fd);
+	info_header(height, width, fd);
+	i = height;
+	while (i-- >= 0)
+		write(fd, image + (i * width * 4), width * 4);
+	close(fd);
 }
