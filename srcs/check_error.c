bd1a1bd229ba908d6c1b2aaffba1e8ad9ab15ca8
Raphael Gilles <rgilles@student.42.fr>
2021-02-25 16:17:22 +0100

    lmao

--- srcs/check_error.c
+++ srcs/check_error.c
@@ -1,13 +1,13 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   check_error.c                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rgilles <rgilles@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/19 20:33:52 by rgilles           #+#    #+#             */
-/*   Updated: 2021/02/19 20:34:04 by rgilles          ###   ########.fr       */
-/*                                                                            */
+/*																																						*/
+/*																												:::			::::::::	 */
+/*	 check_error.c																			:+:			:+:		:+:	 */
+/*																										+:+ +:+				 +:+		 */
+/*	 By: rgilles <rgilles@student.42.fr>						+#+	+:+			 +#+				*/
+/*																								+#+#+#+#+#+	 +#+					 */
+/*	 Created: 2021/02/19 20:33:52 by rgilles					 #+#		#+#						 */
+/*	 Updated: 2021/02/19 20:34:04 by rgilles					###	 ########.fr			 */
+/*																																						*/
 /* ************************************************************************** */
 
 #include <cub3d.h>
@@ -35,5 +35,69 @@ void	check_error(t_vars *mywin)
 		ft_printf("Falling back to current resolution\n");
 		mywin->params.res_x = max_x;
 		mywin->params.res_y = max_y;
+	}
+}
+
+void	check_map_closed(t_vars*mywin)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	while (map[++i])
+	{
+		j = 0;
+	}
+}
+
+void floodFillScanline(Uint32* screenBuffer, int w, int h, int x, int y, int newColor, int oldColor)
+{
+	int x1;
+
+	//draw current scanline from start position to the right
+	x1 = x;
+	while(map[y][x1] != '1' && map[y][x1] != '2')
+		x1++;
+	//draw current scanline from start position to the left
+	x1 = x - 1;
+	while(map[y][x1] != '1' && map[y][x1] != '2')
+		x1--;
+	//test for new scanlines above
+	x1 = x;
+	while(x1 < w && screenBuffer[y * w + x1] == newColor)
+	{
+		if(y > 0 && screenBuffer[(y - 1) * w + x1] == oldColor)
+		{
+			floodFillScanline(screenBuffer, w, h, x1, y - 1, newColor, oldColor);
+		}
+		x1++;
+	}
+	x1 = x - 1;
+	while(x1 >= 0 && screenBuffer[y * w + x1] == newColor)
+	{
+		if(y > 0 && screenBuffer[(y - 1) * w + x1] == oldColor)
+		{
+			floodFillScanline(screenBuffer, w, h, x1, y - 1, newColor, oldColor);
+		}
+		x1--;
+	}
+	//test for new scanlines below
+	x1 = x;
+	while(x1 < w && screenBuffer[y * w + x1] == newColor)
+	{
+		if(y < h - 1 && screenBuffer[(y + 1) * w + x1] == oldColor)
+		{
+			floodFillScanline(screenBuffer, w, h, x1, y + 1, newColor, oldColor);
+		}
+		x1++;
+	}
+	x1 = x - 1;
+	while(x1 >= 0 && screenBuffer[y * w + x1] == newColor)
+	{
+		if(y < h - 1 && screenBuffer[(y + 1) * w + x1] == oldColor)
+		{
+			floodFillScanline(screenBuffer, w, h, x1, y + 1, newColor, oldColor);
+		}
+		x1--;
 	}
 }
